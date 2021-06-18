ed58a79a17e0b54c6eba85d03cc4b7aebda0156b
Dmitry Sam <dmitry.sam88@gmail.com>
2021-06-18 11:33:05 +0300

    Add default sql path build

--- dist/index.js
+++ dist/index.js
@@ -3,24 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
 const routineDump_1 = __importDefault(require("./lib/routineDump"));
 const tableDump_1 = __importDefault(require("./lib/tableDump"));
-function default_1(sqlFilesPath, connectionOptions) {
+function default_1(connectionOptions, sqlFilesPath = path_1.default.join(__dirname, '../../sql')) {
+    if (!connectionOptions.connectionLimit)
+        connectionOptions.connectionLimit = 1;
     return {
         ...routineDump_1.default(sqlFilesPath, connectionOptions),
         ...tableDump_1.default(sqlFilesPath, connectionOptions),
     };
 }
 exports.default = default_1;
-// (async function () {
-//   const sqlDirPath = path.join(__dirname, '..', 'sql');
-//   const { saveAllRoutines, saveAllTables, saveAllTablesWithData } = main(sqlDirPath, {
-//     host: '192.168.201.221',
-//     user: 'root',
-//     password: '00!rtvc!00',
-//     connectionLimit: 4
-//   });
-//   await saveAllRoutines();
-//   await saveAllTables();
-// })();
 //# sourceMappingURL=index.js.map
