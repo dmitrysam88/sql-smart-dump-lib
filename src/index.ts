ba94d2b98ee045ca52dbc24c6f2204cdc774421c
Dmitry Sam <dmitry.sam88@gmail.com>
2021-06-18 13:24:42 +0300

    remove dist

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import routineDump from './lib/routineDump';
 import tableDump from './lib/tableDump';
 import { ConnectionObject } from './lib/helper';
 
-export default function (connectionOptions: ConnectionObject, sqlFilesPath: string = path.join(__dirname, '../../sql')) {
+export default function (connectionOptions: ConnectionObject, sqlFilesPath: string = path.join(__dirname, '../../../sql')) {
   if (!connectionOptions.connectionLimit) connectionOptions.connectionLimit = 1;
 
   return {
