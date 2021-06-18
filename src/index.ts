3a5c6aeeb352b488fafb03e6ac6d8f43b942bb14
Dmitry Sam <dmitry.sam88@gmail.com>
2021-06-18 11:30:00 +0300

    Default sqlFilesPath was added into library

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,14 @@
 import path from 'path';
 
-import mysql, { Pool } from 'mysql2/promise';
-
 import routineDump from './lib/routineDump';
 import tableDump from './lib/tableDump';
 import { ConnectionObject } from './lib/helper';
 
-export default function (sqlFilesPath: string, connectionOptions: ConnectionObject) {
+export default function (connectionOptions: ConnectionObject, sqlFilesPath: string = path.join(__dirname, '../../sql')) {
+  if (!connectionOptions.connectionLimit) connectionOptions.connectionLimit = 1;
+
   return {
     ...routineDump(sqlFilesPath, connectionOptions),
     ...tableDump(sqlFilesPath, connectionOptions),
   }
 }
-
-// (async function () {
-//   const sqlDirPath = path.join(__dirname, '..', 'sql');
-
-//   const { saveAllRoutines, saveAllTables, saveAllTablesWithData } = main(sqlDirPath, {
-//     host: '192.168.201.221',
-//     user: 'root',
-//     password: '00!rtvc!00',
-//     connectionLimit: 4
-//   });
-
-//   await saveAllRoutines();
-//   await saveAllTables();
-// })();
