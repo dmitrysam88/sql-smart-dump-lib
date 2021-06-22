f4852468b3bf990a397d1827780508331aa8bc6f
Dmitry Sam <dmitry.sam88@gmail.com>
2021-06-22 16:05:31 +0300

    add result type

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,23 @@ import loadRoutine from './lib/loadRoutine';
 import loadTable from './lib/loadTable';
 import { ConnectionObject } from './lib/helper';
 
+type SQLSmartDump = {
+  saveAllRoutines?: Function;
+  saveAllTables?: Function; 
+  saveAllTablesWithData?: Function;
+  loadAllRoutines?: Function;
+  loadAllTables?: Function;
+}
+
 export default function (connectionOptions: ConnectionObject, sqlFilesPath: string = path.join(__dirname, '../../../sql')) {
   if (!connectionOptions.connectionLimit) connectionOptions.connectionLimit = 1;
 
-  return {
+  const sqlSmartDump: SQLSmartDump = {
     ...routineDump(sqlFilesPath, connectionOptions),
     ...tableDump(sqlFilesPath, connectionOptions),
     ...loadRoutine(sqlFilesPath, connectionOptions),
     ...loadTable(sqlFilesPath, connectionOptions),
   }
+
+  return sqlSmartDump;
 }
