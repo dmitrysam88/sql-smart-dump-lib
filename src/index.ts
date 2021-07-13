f9c00b70d7f57d2ccf8419b8b83d262804959ee1
Dmitry Sam <dmitry.sam88@gmail.com>
2021-07-13 14:20:42 +0300

    use exports all

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import RoutineDumper from './lib/RoutineDumper';
-import TableDumper from './lib/TableDumper';
-import EventDumper from './lib/EventDumper';
-import ElementLoader from './lib/ElementLoader';
+// import RoutineDumper from './lib/RoutineDumper';
+// import TableDumper from './lib/TableDumper';
+// import EventDumper from './lib/EventDumper';
+// import ElementLoader from './lib/ElementLoader';
 
-export default { RoutineDumper, TableDumper, EventDumper, ElementLoader };
+export * from './lib/RoutineDumper';
+export * from './lib/TableDumper';
+export * from './lib/EventDumper';
+export * from './lib/ElementLoader';
+
+// export default { RoutineDumper, TableDumper, EventDumper, ElementLoader };
